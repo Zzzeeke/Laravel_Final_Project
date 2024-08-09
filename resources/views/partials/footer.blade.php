ecf2cd0c0a5a2ce144d0c97e9ca6bdd15db27b4d
jmaynigo <jpm2242@students.uc-bcf.edu.ph>
2024-08-10 00:07:46 +0800

    Creating the header and footer for the dashboard.

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container mx-auto text-center">
+        <p>&copy; {{ date('Y') }} Cagas, Ezekiel and Maynigo, Janelle. All Rights Reserved</p>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="container mx-auto">
+        <h1 class="text-2xl">ToDo List</h1>
+    </div>
+</header>
