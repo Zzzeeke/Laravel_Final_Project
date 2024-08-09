b6c6839c08569ee8eccb9851a42ada01dd9a0e7d
Zzzeeke <zekecagas@gmail.com>
2024-08-10 00:12:59 +0800

    configuring routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Create_ToDoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// CRUD routes
+
+Route::get('/todos/create', [Create_ToDoController::class, 'create'])->name('create_todo');
+Route::post('/todos', [Create_ToDoController::class, 'store'])->name('todos.store');
+Route::get('/dashboard', [Create_ToDoController::class, 'dashboard'])->name('dashboard');
+Route::post('/todos/{id}/complete', [Create_ToDoController::class, 'markCompleted'])->name('todos.complete');
 
 require __DIR__.'/auth.php';
